ac91d9b6dc6514ec545358db86f4f42a41d10188
iguigu <iguchi2203@gmail.com>
2025-08-16 15:01:44 +0900

    teacher モデルとテーブル作成済み

--- src/app/Models/Teacher.php
+++ src/app/Models/Teacher.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens,HasFactory,Notifiable;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
