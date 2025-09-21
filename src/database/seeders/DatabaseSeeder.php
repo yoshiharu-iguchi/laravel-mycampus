377606078ad5be5c93422122ff631f72d0ef8dd4
iguigu <iguchi2203@gmail.com>
2025-09-21 13:16:17 +0900

    databaseseederを追加保存

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             RosterStudentSeeder::class,
             SubjectSeeder::class,
             DemoEnrollmentSeeder::class,
+            FacilitySeeder::class,
         ]);
     }
 }
