963530de70cf29ff6ca15e354e83a69e22e657fc
iguigu <iguchi2203@gmail.com>
2025-10-24 16:26:55 +0900

    コントローラ修正

--- src/app/Http/Controllers/Student/Auth/AuthenticatedSessionController.php
+++ src/app/Http/Controllers/Student/Auth/AuthenticatedSessionController.php
@@ -41,6 +41,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/student/login');
+        return redirect()->route('login');
     }
 }
